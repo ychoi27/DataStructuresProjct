79865c6f3b559c078fc652e3a85c0e5d3d7c585a
att2976 <att2976@att.net>
2016-11-28 16:45:23 -0500

    Add files via upload
    added constructors getters setters

--- UserInfo.java
+++ UserInfo.java
@@ -1,9 +1,61 @@
+public class UserInfo
+{
+	private String name,address,email;
+	private int id, phone;
 
-public class UserInfo {
-	
-	private String name, adress, email;
-	private int phoneNumber;
-	
-	
+      // default constructor
+      public UserInfo()
+      {       
+         name = "";
+         address = "";
+         email = "";
 
-}
+         id = 0;  
+         phone = 0;
+      }
+
+      //constructor with all values 
+	public UserInfo(String name, String address, int phone, String email) {
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+
+      // setters
+
+	public void setName(String n){
+		name=n;		
+	}
+	
+	public void setAddress(String a){
+		address=a;
+	}
+	
+	public void setPhone(int ph){
+		phone=ph;
+	}
+	
+	public void setEmail(String e){
+		email=e;
+	}
+
+      // getters
+
+	public String getName(){
+		return name;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+	public int getPhone(){
+		return phone;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+} 
